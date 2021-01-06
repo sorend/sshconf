4790159842efb695b50b4af26ed7c31f55552a1b
Soren A D <sorend@gmail.com>
2021-01-06 22:28:02 +0100

    added version.txt to the distribution so setuptools pickup the version when installing

--- setup.py
+++ setup.py
@@ -20,11 +20,12 @@ setup(
     py_modules=['sshconf'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
-    classifiers=(
+    data_files=['version.txt'],
+    classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Libraries',
-    )
+    ]
 )
