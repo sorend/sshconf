6df1242f45e0166a6e3bcae514668cd36da5c91d
alingse <alingse@foxmail.com>
2022-01-20 21:21:53 +0800

    remove pytest

--- setup.py
+++ setup.py
@@ -18,8 +18,6 @@ setup(
     license='MIT',
     keywords=['ssh', 'config'],
     py_modules=['sshconf'],
-    setup_requires=[],
-    tests_require=['pytest-runner', 'pytest'],
     data_files=['version.txt'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
